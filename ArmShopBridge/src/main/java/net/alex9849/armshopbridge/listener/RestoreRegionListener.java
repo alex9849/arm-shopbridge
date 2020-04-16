71da599d5d03b2929a0f0aee928e25cc6a5317be
alex9849 <alexander@liggesmeyer.de>
2020-04-16 08:13:35 +0200

    added support for new listener

--- ArmShopBridge/src/main/java/net/alex9849/armshopbridge/listener/RestoreRegionListener.java
+++ ArmShopBridge/src/main/java/net/alex9849/armshopbridge/listener/RestoreRegionListener.java
@@ -1,6 +1,6 @@
 package net.alex9849.armshopbridge.listener;
 
-import net.alex9849.arm.events.ResetBlocksEvent;
+import net.alex9849.arm.events.RestoreRegionEvent;
 import net.alex9849.armshopbridge.ArmShopBridge;
 import net.alex9849.armshopbridge.interfaces.IShopPluginAdapter;
 import org.bukkit.event.EventHandler;
@@ -10,7 +10,7 @@ import org.bukkit.event.Listener;
 public class RestoreRegionListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void handleRegionReset(ResetBlocksEvent event) {
+    public void handleRegionReset(RestoreRegionEvent event) {
         for(IShopPluginAdapter adapter : ArmShopBridge.getInstance().getShopPluginAdapters()) {
             adapter.deleteShops(event.getRegion());
         }
